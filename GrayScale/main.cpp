9bec56ae0e3b9adc287c387c7f2cf34f172acf53
cjsauer18 <57496346+cjsauer18@users.noreply.github.com>
2020-03-07 17:22:32 -0500

    Update main.cpp

--- GrayScale/main.cpp
+++ GrayScale/main.cpp
@@ -8,9 +8,9 @@ using std::cout; using std::endl; using std::vector;
 #include "GrayScale.h"
 
 int main(){
-    Image image("test.pgm");
-    image.write_image("test.txt");
-    auto image_test = image.sharpen();
+    Image image("test.pgm");//instantiate by giving file name
+    image.write_image("test.txt");//writes out to a file and will be named "test"
+    auto image_test = image.sharpen();//convolving to sharpen
     image_test.write_image("image_test.txt");
     std::ifstream input;
     input.open("file.txt");
